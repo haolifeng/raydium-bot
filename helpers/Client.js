11d5f6ac9e6fcbe36a060eb33022e1fd51975e0a
haolifeng <haolifengwang>
2025-06-13 10:00:58 +0800

    update

--- helpers/Client.js
+++ helpers/Client.js
@@ -1,11 +1,171 @@
 const { Connection } = require('@solana/web3.js');
+const CONN_TIME_OUT = 10 * 30* 1000 * 2;
+const  { TimeoutPromise } = require('../helpers/common')
 class Client {
     constructor(url) {
         this.url = url;
         this.connection = new Connection(this.url);
+        this.logger = global.appLogger;
     }
     getConnection() {
         return this.connection
+    }
+    async getBalance(pubKey) {
+        try {
+            const reqPromise =this.connection.getBalance(pubKey);
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getBalance, e: ",e );
+            throw e;
+        }
+    }
+    async getBlockHeight() {
+        try{
+            const reqPromise =   this.connection.getSlot({commitment:"finalized"});
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getBlockHeight, e: ",e );
+            throw e;
+        }
+
+    }
+    async getBlock(slot) {
+        try{
+            const reqPromise =    this.connection.getBlock(slot, {commitment: "finalized", maxSupportedTransactionVersion: 0, rewards:false});
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getBlock, e: ",e );
+            throw e;
+        }
+
+
+    }
+    async getBlocks(startSlot, endSlot) {
+        try{
+            const reqPromise =      this.connection.getBlocks(startSlot, endSlot, {commitment: "finalized", maxSupportedTransactionVersion: 0, rewards:false});
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getBlock, e: ",e );
+            throw e;
+        }
+
+
+    }
+
+    async getTransaction(signature,commitment='finalized') {
+        try {
+            const reqPromise  = this.connection.getTransaction(signature, {commitment:commitment, maxSupportedTransactionVersion: 0});
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getTransaction, e: ",e );
+            throw e;
+        }
+
+    }
+    async getAddressLookupTable(lookUptablePk) {
+        try {
+            const reqPromise  = this.connection.getAddressLookupTable(lookUptablePk);
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getAddressLookupTable, e: ",e );
+            throw e;
+        }
+    }
+
+    async getSignatureStatus(signaure) {
+        try {
+            const reqPromise  = this.connection.getSignatureStatus(signature);
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("getSignatureStatus, e: ",e );
+            throw e;
+        }
+    }
+    async sendTransaction(transaction, keypair) {
+        try {
+
+            const reqPromise  =  this.connection.sendTransaction( transaction, keypair);
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error("sendTransaction, e: ",e );
+            throw e;
+        }
+
+    }
+    async sendRawTransaction(rawTx) {
+        try {
+
+            const reqPromise = this.connection.sendRawTransaction(rawTx);
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error('sendRawTransaction, e: ',e);
+            throw  e;
+        }
+
+    }
+    async getLatestBlockhash() {
+        try{
+            let reqPromise = this.connection.getLatestBlockhash();
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }
+        catch (e) {
+            this.logger.error('getLatestBlockhash, e: ',e);
+            throw  e;
+        }
+    }
+    async getAccountInfo(publicKey) {
+        try{
+            let reqPromise = this.connection.getAccountInfo(publicKey, {commitment:"finalized"});
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+
+            return ret;
+        }catch (e) {
+            this.logger.error('getAccountInfo, e:',e);
+            throw e;
+        }
+    }
+    async getSignaturesForAddress(programPubKey,until,before,limit){
+
+        try{
+            let reqPromise = this.connection.getSignaturesForAddress(programPubKey,{ until:until,before:before, limit:limit},"finalized");
+            const reqPromiseWithTimeout = new TimeoutPromise(reqPromise, CONN_TIME_OUT);
+            let ret = await reqPromiseWithTimeout;
+            return ret;
+        }catch (e) {
+            this.logger.error('getSignaturesForAddress, e:',e);
+            throw e;
+        }
     }
 }
 module.exports = Client;
--- helpers/common.js
+++ helpers/common.js
@@ -1,3 +1,7 @@
+function isPromise(p) {
+    return p && Object.prototype.toString.call(p) === "[object Promise]";
+}
+
 function sleep(time) {
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
@@ -6,7 +10,45 @@ function sleep(time) {
     })
 }
 
+class TimeoutPromise extends Promise {
+    constructor(cbOrPromise, ms = 30 * 1000, hint = 'PTIMEOUT', exitCondition) {
+        let timeout;
+        let wrapperPromise = Promise.race([
+            isPromise(cbOrPromise) ? cbOrPromise : new Promise(cbOrPromise),
+            new Promise((resolve, reject) => {
+                timeout = setTimeout(() => {
+                    if (exitCondition) {
+                        exitCondition.exist = true;
+                    }
+                    reject(new Error(hint));
+                }, ms);
+            }),
+        ]);
+
+        return wrapperPromise.then((data) => {
+            clearTimeout(timeout);
+            return data;
+        }).catch((error) => {
+            clearTimeout(timeout);
+            // if (exitCondition) {
+            //     exitCondition.exist = true;
+            // }
+            throw error; // if timeout, reject the hint error
+        })
+
+        //   super((resolve, reject) => {
+        //       wrapperPromise.then((data) => {
+        //           clearTimeout(timeout);
+        //           resolve(data);
+        //       }).catch((error) => {
+        //           clearTimeout(timeout);
+        //           reject(error); // if timeout, reject the hint error
+        //       })
+        //   });
+    }
+}
+
 
 module.exports = {
-    sleep
+    sleep,TimeoutPromise
 }
