6697233f1f4204d681df9cf4d588518e3c828067
haolifeng <haolifengwang>
2025-06-14 10:09:54 +0800

    update

--- config/Config.js
+++ config/Config.js
@@ -1,8 +1,20 @@
+const {SOLMint,WSOLMint, USDCMint} = require("@raydium-io/raydium-sdk-v2");
+
 let config = {
     nodeUrl:"https://api.devnet.solana.com",
 
     checkInterval:50* 1000,
-
+    pool:{
+        poolId:"3ucNos4NbumPLZNWztqGHNFFgkHeRMBQAVemeeomsUxv",
+        mintA:{
+            scAddr:WSOLMint,
+            softMaxLimit:10000 * 1000000
+        },
+        mintB:{
+            scAddr:USDCMint,
+            softMaxLimit:10000 * 1000000
+        },
+    },
     market:{
         gasMiniLimit:10000,
 
--- rules/BalanceRule.js
+++ rules/BalanceRule.js
@@ -1,5 +1,6 @@
 const RuleInterface = require("./ruleInterface");
 const {sleep } = require("../helpers/common")
+const {WSOLMint} = require("@raydium-io/raydium-sdk-v2");
 class BalanceRule extends RuleInterface{
     constructor(owner, client, config) {
         super();
@@ -20,6 +21,7 @@ class BalanceRule extends RuleInterface{
         }
     };
     async checkCondition(){
+
         let gasMiniLimit = this.config.market.gasMiniLimit;
 
         let ownerAddr = this.owner.publicKey
@@ -33,6 +35,42 @@ class BalanceRule extends RuleInterface{
 
 
     } ;
+    async checkGasSolBalance(){
+
+        let gasMiniLimit = this.config.market.gasMiniLimit;
+
+        let ownerAddr = this.owner.publicKey
+        let solBalance =await  this.client.getBalance(ownerAddr);
+
+        if(solBalance < gasMiniLimit){
+            this.logger.error("current, sol balance is too low, need to add balance, ", "ownerAddr: ", ownerAddr.toBase58(), "current solBalance: ", solBalance, "gasMiniLimit: ", gasMiniLimit);
+        }else{
+            this.logger.info("current, sol balance is normal, don't worry, ownerAddr: ", ownerAddr.toBase58(), "current solBalance: ", solBalance, "gasMiniLimit: ", gasMiniLimit);
+        }
+
+    }
+    async checkMintBalance(){
+        let mintA = this.config.pool.mintA.scAddr;
+        let mintB = this.config.pool.mintB.scAddr;
+        let mintABalance = 0;
+        if(mintA === WSOLMint){
+             mintABalance =await  this.client.getBalance(this.owner.publicKey);
+        }else{
+             mintABalance = await this.client.getTokenBalance(this.owner.publicKey, mintA);
+        }
+        if(mintABalance > this.config.pool.mintA.softMaxLimit){
+
+        }
+        let mintBBalance = 0;
+        if(mintB === WSOLMint){
+             mintBBalance =await  this.client.getBalance(this.owner.publicKey);
+        }else{
+             mintBBalance = await this.client.getTokenBalance(this.owner.publicKey, mintB);
+        }
+        if(mintBBalance > this.config.pool.mintB.softMaxLimit) {
+
+        }
+    }
     async perform(){
 
     };
